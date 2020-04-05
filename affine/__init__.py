83d3c40e7dee1134a483ab0d3bceafaf52a3184f
Minh Nguyen <15853@student.vgu.edu.vn>
2020-04-05 20:31:52 +0700

    Update __init__.py

--- affine/__init__.py
+++ affine/__init__.py
@@ -1,3 +1,1 @@
-import numpy as np
-import matplotlib as plt
-from matplotlib.animation import FuncAnimation
+print('Message from Affine: You are using Affine - a module for visualization of affine transformations in 2-dimensional Euclidean space.')
