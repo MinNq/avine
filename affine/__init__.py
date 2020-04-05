612eb3f05e8320b4c594a55ced447f1812997d5a
Minh Nguyen <15853@student.vgu.edu.vn>
2020-04-05 20:33:58 +0700

    Update __init__.py

--- affine/__init__.py
+++ affine/__init__.py
@@ -1,1 +1,1 @@
-print('Message from Affine: You are using Affine - a module for visualization of affine transformations in 2-dimensional Euclidean space.')
+print('Message from Affine: You are using Affine - a module for visualization of affine transformations in 2-d Euclidean space.')
