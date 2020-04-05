273bab5fdc88514957d89e13586d071c399fe9d7
Minh Nguyen <15853@student.vgu.edu.vn>
2020-04-05 20:42:36 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='Affine',
-    url='https://github.com/MinNq/affine',
+    name='AffineVi',
+    url='https://github.com/MinNq/affinevi',
     author='Minh Nguyen',
     author_email='nguyenquangminhptnk@gmail.com',
-    packages=['affine'],
+    packages=['affinevi'],
     install_requires=['numpy', 'matplotlib'],
     version='0.1',
     license='MIT',
-    description='Affine - a module for visualization of affine transformations in 2-d Euclidean space',	
+    description='AffineVi - a module for visualization of affine transformations in 2-d Euclidean space',	
     long_description=open('README.txt').read(),
 )
