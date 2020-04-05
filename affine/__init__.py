3ae1e97a920dd014a52f9df6a5c42f6bd603f142
Minh Nguyen <15853@student.vgu.edu.vn>
2020-04-05 20:43:09 +0700

    Update and rename affine/__init__.py to affinevi/__init__.py

--- affine/__init__.py
+++
@@ -1,1 +0,0 @@
-print('Message from Affine: You are using Affine - a module for visualization of affine transformations in 2-d Euclidean space.')
---
+++ affinevi/__init__.py
@@ -0,0 +1,1 @@
+print('Message from AffineVi: You are using AffineVi - a module for visualization of affine transformations in 2-d Euclidean space.')
