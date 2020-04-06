73d4dbafad58c00044aa4380a9cbc0e0b2b957f1
Minh Nguyen <15853@student.vgu.edu.vn>
2020-04-07 03:31:55 +0700

    Add more functions, reorganize codes
    ShowSeries() is added.

--- avine/Visualize.py
+++ avine/Visualize.py
@@ -1,135 +1,237 @@
+from __future__ import print_function
+
 import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('seaborn')
 from matplotlib.animation import FuncAnimation
-from avine.Transformations import * 
+
+from Transformations import *
 
 
-'''
-ShowTransformations(data, gif_length = 1, fps = 60)
+def ShowTransformations(data, save_at, gif_length = 1, fps = 30):
 
-> create a gif with all defined affine transformations 
-applied to given data
+    '''
+    > Show all defined affine transformations applied to
+    given data in a gif.
 
-data: array_like. Coordinates in 2-d Cartesian space.
-gif_length: int or float. Length of ouput gif in second. 
-fps: int. Frame rate of output gif.
-'''
+    Parameters:
+    - data: array_like. A matrix where each column
+    is an augmented data point.
+    - save_at: string. The link to where you want to save
+    your gif.
+    - gif_length (optional): float. Length of the ouput
+    gif.
+    - fps (optional): int. Frame rate of the output gif.
+    '''
 
-def ShowTransformations(data, gif_length = 1, fps = 60):
+    # preprocessing
+    Data = augmented(data)
+    
+    # preparing figure and axes
+    fig, axs = plt.subplots(3, 3, figsize = (8,8))
+    plt.subplots_adjust(top = 0.8, wspace = 0.3, hspace = 0.4)
+    plt.suptitle('Affine Transformations', y = 0.93,
+                    size = 'x-large', weight = 'bold')
 
-	# creating a figure and a bunch of subplots
-	fig = plt.figure(figsize = (8, 8))
-	plt.subplots_adjust(top = 0.8, wspace = 0.3, hspace = 0.4)
-	plt.suptitle('Affine Transformations', y = 0.93,
-					size = 'x-large', weight = 'bold')
+    lim = max(np.abs(np.max(Data))*.5, 1) + np.abs(np.max(Data))
+    margin = .25
 
-	axs = [] # a list of axes
-	artists = [] # a list of artists
-	for subplot_id in range(9):
-		axs.append(plt.subplot(3, 3, subplot_id + 1))
-		artists.append(axs[subplot_id].scatter([], []))
+    # initializing subplots
+    plots = []
+    for i in range(9):
+        axs[i/3, i%3].set(xlim=(-lim - margin, lim + margin),
+                            ylim=(-lim - margin, lim + margin))
+        # highlighting x-axis and y-axis
+        axs[i/3, i%3].axhline(linewidth = 1, color = 'gray')
+        axs[i/3, i%3].axvline(linewidth = 1, color = 'gray')
+        plots.append(axs[i/3, i%3].scatter([], [], s = 15,
+                        color = 'red', alpha = .5))
 
-	# titles for the subplots
-	titles = ['No change']
-	titles.append('Translate')
-	titles.append('Scale about origin',)
-	titles.append('Rotate about origin')
-	titles.append('Shear in $x$ direction')
-	titles.append('Shear in $y$ direction')
-	titles.append('Reflect about origin')
-	titles.append('Reflect about $x$-axis')
-	titles.append('Reflect about $y$-axis')
+    # identity
+    plots[0].set_offsets(np.c_[Data[0, :], Data[1, :]])
+    axs[0, 0].set_title('Translation')
 
-	# preprocessing
-	augmented_data = [augmented(vector) for vector in data]
+    # animation function for FuncAnimation
+    def animate(frame):
 
-	distances = [np.dot(vector[:2].T, vector[:2]) for vector in augmented_data]
-	lim = np.max(distances) + max(.5*np.max(distances), 1.5)
+        # a float running from 0 to 1 to track progress
+        progress = float(frame)/(fps*gif_length)
 
-	# clearing all axes and reseting their appearance
-	def reset():
-		for subplot_id in range(1, 9):
+        # translation
+        update = transformations[1](Data, [progress, progress])
+        plots[1].set_offsets(np.c_[update[0, :], update[1, :]])
+        axs[0, 1].set_title('Translation')
 
-			axs[subplot_id].cla()
+        # scaling
+        scale_factor = progress*(np.array([1.5, 1.2]) - 1) + 1
+        update = transformations[2](Data, scale_factor)
+        plots[2].set_offsets(np.c_[update[0, :], update[1, :]])
+        axs[0, 2].set_title('Scaling')
 
-			axs[subplot_id].set_title(titles[subplot_id])
-			axs[subplot_id].set_xlim(-lim, lim)
-			axs[subplot_id].set_ylim(-lim, lim)
-			axs[subplot_id].grid(True)
+        # rotation around origin
+        update = transformations[3](Data, progress*np.pi/3)
+        plots[3].set_offsets(np.c_[update[0, :], update[1, :]])
+        axs[1, 0].set_title('Rotation around origin')
 
-			# highlighting x-axis and y-axis
-			axs[subplot_id].axhline(linewidth = 1, color = 'gray')
-			axs[subplot_id].axvline(linewidth = 1, color = 'gray')
+        # shearing in x direction
+        update = transformations[4](Data, progress*np.pi/3)
+        plots[4].set_offsets(np.c_[update[0, :], update[1, :]])
+        axs[1, 1].set_title('Shearing in $x$ direction')
 
-	# identity
-	for subplot_id in range(0, 1):	
+        # shearing in y direction
+        update = transformations[5](Data, progress*np.pi/3)
+        plots[5].set_offsets(np.c_[update[0, :], update[1, :]])
+        axs[1, 2].set_title('Shearing in $y$ direction')    
 
-		axs[subplot_id].set_title(titles[subplot_id])
-		axs[subplot_id].set_xlim(-lim, lim)
-		axs[subplot_id].set_ylim(-lim, lim)
-		axs[subplot_id].grid(True)
+        # reflection through origin
+        condition = int(progress*gif_length/float(0.2))%2
+        update = transformations[6](Data) if condition else Data
+        plots[6].set_offsets(np.c_[update[0, :], update[1, :]])
+        axs[2, 0].set_title('Reflection through origin')          
 
-		# highlighting x-axis and y-axis
-		axs[subplot_id].axhline(linewidth = 1, color = 'gray')
-		axs[subplot_id].axvline(linewidth = 1, color = 'gray')
+        # reflection through x-axis
+        condition = int(progress*gif_length/float(0.2))%2
+        update = transformations[7](Data) if condition else Data
+        plots[7].set_offsets(np.c_[update[0, :], update[1, :]])
+        axs[2, 1].set_title('Reflection through origin')  
 
-		to_show = np.array([func_list[subplot_id](vector) 
-							for vector in augmented_data])
-		axs[subplot_id].scatter(to_show[:, 0], to_show[:, 1], 
-								color = 'red', s = 20, alpha = 0.5)
-
-	# initialization function for FuncAnimation
-	def init():
-		artists.set_data([], [])
-		return artists,
-
-	# animation function for FuncAnimation
-	def animate(frame):
-
-		progress = float(frame)/(fps*gif_length)
-
-		if frame:
-			print('Progress: {}%'.format(progress*90), flush = True)
-
-		reset()
-
-		# translation and scaling
-		for subplot_id in range(1, 3):
-			
-			to_show = np.array([func_list[subplot_id](vector, [progress*(-.5), progress*1.5]) 
-								for vector in augmented_data])
-			axs[subplot_id].scatter(to_show[:, 0], to_show[:, 1],
-									color = 'red', s = 20, alpha = 0.5)
-
-		# rotation and shearing
-		for subplot_id in range(3, 6):
-			
-			to_show = np.array([func_list[subplot_id](vector, progress*np.pi/3) 
-								for vector in augmented_data])
-			axs[subplot_id].scatter(to_show[:, 0], to_show[:, 1],
-									color = 'red', s = 20, alpha = 0.5)
-
-		# reflection
-		for subplot_id in range(6, 9):
-
-			if int(progress*10)%2:
-				to_show = np.array([func_list[subplot_id](vector) 
-								for vector in augmented_data])
-			else:
-				to_show = np.array([vector for vector in augmented_data])
-
-			axs[subplot_id].scatter(to_show[:, 0], to_show[:, 1],
-									color = 'red', s = 20, alpha = 0.5)
+        # reflection through y-axis
+        condition = int(progress*gif_length/float(0.2))%2
+        update = transformations[8](Data) if condition else Data
+        plots[8].set_offsets(np.c_[update[0, :], update[1, :]])
+        axs[2, 2].set_title('Reflection through origin')
 
 
-		return artists
+    anim = FuncAnimation(fig, animate, interval = float(1000)/fps, 
+                            frames = fps*gif_length)
 
-	# making animation with FuncAnimation
-	anim = FuncAnimation(fig, animate, frames = int(fps*gif_length),
-						interval = float(1000)/fps, blit = True)
+    anim.save(save_at, writer = 'imagemagick', dpi = 200)
 
-	# saving the animation into a gif with ImageMagick
-	anim.save('~/affine_transformations.gif', writer='imagemagick', dpi = 200)
+    print('Your gif is ready! Check it at {}.'.format(save_at))
 
-	print('Done! Check your gif!', flush = True)
+def ShowSeries(data, transformation_list, save_at, gif_length = 3, fps = 30):
+
+    '''
+    > Show respectively all given affine transformations 
+    applied to given data in a gif.
+
+    Parameters:
+    - data: array_like. A matrix where each column
+    is an augmented data point.
+    - transformation_list: array_like. A list of
+    transformations to apply, including
+    transformation codes (from 0 to 8) and 
+    corresponding parameters in a 2-tuple. If
+    there are no parameters, just use an integer.
+    - save_at: string. The link to where you want to save
+    your gif.
+    - gif_length (optional): float. Length of the ouput
+    gif.
+    - fps (optional): int. Frame rate of the output gif.
+    '''
+
+    # preprocessing
+    Data = augmented(data)
+
+    # adding an identity to the start
+    transformation_list.insert(0, (0,0))
+
+    # standardizing
+    transformation_list = standardize(transformation_list)
+
+    # outputs from each component transformation
+    to_do = np.array(series(Data, transformation_list))
+    
+    # preparing figure
+    fig = plt.figure(figsize = (5,5))
+    plt.subplots_adjust(top = 0.8, wspace = 0.3, hspace = 0.4)
+    plt.suptitle('Affine Transformation Series', y = 0.93, weight = 'bold')
+    
+    # highlighting x-axis and y-axis
+    plt.axhline(linewidth = 1, color = 'gray')
+    plt.axvline(linewidth = 1, color = 'gray')
+
+    margin = .25
+    x_lim = (np.min(to_do[:, 0]) - margin, np.max(to_do[:, 0]) + margin)
+    y_lim = (np.min(to_do[:, 1]) - margin, np.max(to_do[:, 1]) + margin)
+
+    # initializing plot
+    plt.xlim(x_lim)
+    plt.ylim(y_lim)
+    plot = plt.scatter([], [], s = 25, color = 'red', alpha = .5)
+
+    # checkpoints for turning into the next transformation
+    n = len(transformation_list)
+    checkpoints = np.arange(0, 1, float(1)/n) + float(1)/n
+
+    # starting with identity
+    #plt.scatter(Data[0, :], Data[1, :], s = 25, color = 'blue', alpha = .5)
+    plot.set_offsets(np.c_[Data[0, :], Data[1, :]])
+
+    # animation function for FuncAnimation
+    def animate(frame):
+
+        # a float running from 0 to 1 to track progress
+        progress = float(frame)/(fps*gif_length)
+
+        # looping through checkpoints to catch the right
+        # transformation
+        for point in range(n):
+            if progress < checkpoints[point] and progress + float(1)/n > checkpoints[point]:
+
+                plt.title('Step {}: {}'.format(point, transformation_list[point]))
+
+                unit_progress = n*(progress - checkpoints[point]) + 1
+
+                # translation
+                if transformation_list[point][0] == 1:
+                    update = transformations[1](to_do[point], [unit_progress, unit_progress])
+                    plot.set_offsets(np.c_[update[0, :], update[1, :]])
+
+                # scaling
+                elif transformation_list[point][0] == 2:
+                    arg = np.array(transformation_list[point][1])
+                    scale_factor = unit_progress*(arg - 1) + 1
+                    update = transformations[2](to_do[point], scale_factor)
+                    plot.set_offsets(np.c_[update[0, :], update[1, :]])
+
+                # rotation around origin
+                elif transformation_list[point][0] == 3:
+                    arg = transformation_list[point][1]
+                    update = transformations[3](to_do[point], unit_progress*arg)
+                    plot.set_offsets(np.c_[update[0, :], update[1, :]])
+
+                # shearing in x direction
+                elif transformation_list[point][0] == 4:
+                    arg = transformation_list[point][1]
+                    update = transformations[4](to_do[point], unit_progress*arg)
+                    plot.set_offsets(np.c_[update[0, :], update[1, :]])
+
+                # shearing in y direction
+                elif transformation_list[point][0] == 5:
+                    arg = transformation_list[point][1]
+                    update = transformations[5](to_do[point], unit_progress*arg)
+                    plot.set_offsets(np.c_[update[0, :], update[1, :]]) 
+                
+                # reflection through origin
+                elif transformation_list[point][0] == 6:
+                    update = transformations[6](to_do[point])
+                    plot.set_offsets(np.c_[update[0, :], update[1, :]])          
+
+                # reflection through x-axis
+                elif transformation_list[point][0] == 7:
+                    update = transformations[7](to_do[point])
+                    plot.set_offsets(np.c_[update[0, :], update[1, :]])
+
+                # reflection through y-axis
+                elif transformation_list[point][0] == 8:
+                    update = transformations[8](to_do[point])
+                    plot.set_offsets(np.c_[update[0, :], update[1, :]])
+
+
+    anim = FuncAnimation(fig, animate, interval = float(1000)/fps, 
+                            frames = fps*gif_length)
+
+    anim.save(save_at, writer = 'imagemagick', dpi = 200)
+
+    print('Your gif is ready! Check it at {}.'.format(save_at))
